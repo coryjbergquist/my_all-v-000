6b3055ef8aa8a4844954c7a34a6684ba783bbb2d
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-17 21:33:18 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   x = 0
   block_return_values = []
   while x < collection.count
-   array << yield(collection[0])
+   block_return_values << yield(collection[0])
 
     x += 1
   end
